d813d6bd14fa18a546b5b75f7c00d889978a71a7
Jo53 <jose@192.168.0.3>
2025-01-22 02:03:34 -0500

    añadí aplicación web

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header("Bienvenido a mi aplicación de análisis de datos")
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+st.header("Aplicación de Vehículos Usados")
+
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
@@ -11,9 +13,17 @@ if hist_button: # al hacer clic en el botón
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
 # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-        
+    fig_1 = px.histogram(car_data, x="odometer")
 # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig_1, use_container_width=True)
 
-%run path/to/script.py
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un gráfico de disperisión')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un gráfico de disperisión para las columnas odómetro y precio')
+    
+    fig_2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_2, use_container_width=True)
